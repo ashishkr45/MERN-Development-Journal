ee8a38fd5a928ec16f23b4c9171c2db428d1550c
Ashish Kumar <ashishkr45943@gmail.com>
2024-12-21 17:43:53 +0530

    completed the assignment of week 1.1

--- week-1.1/Intro-Js/hello.js
+++ week-1.1/Intro-Js/hello.js
@@ -1,1 +1,26 @@
 console.log("Hello World");
+
+// Assignment #1
+
+function sum(a, b) {
+	return a + b;
+}
+
+let sum1 = sum(35, 34);
+let sum2 = sum("Lucifer"," is_Ashish");
+
+console.log(sum1);
+console.log(sum2);
+
+// Assignment #2
+
+function canVote (age) {
+	if(age == "minor") {
+		return true;
+	}
+	return age > 18;
+}
+
+console.log(canVote(sum1));
+console.log(canVote(4));
+console.log(canVote("minor"));
