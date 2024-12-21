cb9f4aec9a84d06d4795b281602ab3902630f517
Ashish Kumar <ashishkr45943@gmail.com>
2024-12-21 18:32:25 +0530

    learnt input from files

--- week-1.1/Async-JS/index.js
+++ week-1.1/Async-JS/index.js
@@ -1,1 +1,6 @@
-a
+const lmg = require("fs");
+
+const cont1 = lmg.readFileSync("files/a.txt", "utf-8");
+const cont2 = lmg.readFileSync("files/b.txt", "utf-8");
+
+console.log(cont1+ ", " + cont2);
