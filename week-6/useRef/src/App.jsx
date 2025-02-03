b3967b40e28ba70944efcf41a8c9d693a2b02168
Ashish Kumar <ashishkr45943@gmail.com>
2025-02-04 00:07:17 +0530

    learnt bout useRef

--- week-6/useRef/src/App.jsx
+++ week-6/useRef/src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useRef } from 'react'
 import './App.css'
-
+{/*Building a Clock with a start and stop functionality*/}
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentCount, setCurrentCount] = useState(1);
+  const timer = useRef();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  function startClock() {
+    let value = setInterval(function() {
+      setCurrentCount(c => c + 1);
+    }, 1000);
+    timer.current = value;
+  }
+
+  function stopClock() {
+    clearInterval(timer);
+  }
+
+  return ( <div style={{fontSize: "50px"}}>
+    {currentCount} <br />
+    <button onClick={startClock}>Start</button>
+    <button onClick={stopClock}>Stop</button>
+  </div>
   )
 }
 
