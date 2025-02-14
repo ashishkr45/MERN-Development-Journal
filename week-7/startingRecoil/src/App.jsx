b406943c35ad10b0fcb8ef9dd6b9cb0932714bfb
Ashish Kumar <ashishkr45943@gmail.com>
2025-02-14 23:50:47 +0530

    added some selector logic

--- week-7/startingRecoil/src/App.jsx
+++ week-7/startingRecoil/src/App.jsx
@@ -1,45 +1,51 @@
 import './App.css';
 import { RecoilRoot , useRecoilValue, useSetRecoilState } from 'recoil';
-import { counterAtom } from './counter';
+import { counterAtom, evenSelector } from './counter';
 
 function App() {
   return (
-    <div style={{background: "#000", width: "200px"}}>
+    <div style={{background: "#fff", width: "200px"}}>
       <RecoilRoot>
+        <Buttons />
         <Counter />
+        <IsEven />
       </RecoilRoot>
     </div>
   );
 }
 
-function Counter() {
-
-  return <div>
-    <CurrentCount />
-    <Buttons />
-  </div>
-}
-
-function CurrentCount() {
-  const count = useRecoilValue(counterAtom);
-  return <div>Count: {count}</div>;
-}
-
 function Buttons() {
   const setCount = useSetRecoilState(counterAtom);
 
-  function increase() {
-    setCount(c => c + 1)
-  }
+  return (
+    <div>
+      <button onClick={() => {
+        setCount(current => current + 2)
+      }}>Increase</button>
 
-  function decrease() {
-    setCount(c => c - 1)
-  }
+      <button onClick={() => {
+        setCount(current => current - 1)
+      }}>Decrease</button>
+    </div>
+  )
+}
+
+function Counter() {
+  const count = useRecoilValue(counterAtom);
 
   return <div>
-    <button onClick={increase}>Increase</button>
-    <button onClick={decrease}>Decrease</button>
+      <p>{count}</p>
   </div>
+}
+
+function IsEven() {
+  const even = useRecoilValue(evenSelector);
+
+  return (
+    <div>
+      <p>{even ? "Even Digi" : "Odd Digi"}</p>
+    </div>
+  )
 }
 
 export default App;
--- week-7/startingRecoil/src/counter.js
+++ week-7/startingRecoil/src/counter.js
@@ -1,7 +1,17 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export const counterAtom = atom({
     default: 0,
     key: "counter"
 
 });
+
+export const evenSelector = selector({
+    key: "isEvenSelector",
+    // the get function in the parameter throught this
+    // get we can get any atom that we want.
+    get: function({ get }) {
+        const currentCount = get(counterAtom);
+        return currentCount % 2 == 0;
+    }
+})
