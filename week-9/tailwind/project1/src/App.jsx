770e7fcd6585b7d5881d99890526e8a16b4e1170
Ashish Kumar <ashishkr45943@gmail.com>
2025-02-26 20:35:18 +0530

    completed the final look

--- week-9/tailwind/project1/src/App.jsx
+++ week-9/tailwind/project1/src/App.jsx
@@ -3,15 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="bg-[#00274e] h-screen flex flex-col justify-center items-center text-white ">
-      <div className="text-2xl font-bold mb-4 mb-8">Verify Your Age</div>
-      <div className="mb-4 text-center text-sm  text-base text-[#7b9abf]">
+      <div className="text-2xl font-bold mb-8">Verify Your Age</div>
+      <div className="mb-4 text-center text-sm  text-[#7b9abf]">
         Please confirm your birth year. This data will not be stored.
       </div>
       <input
         type="text" placeholder='Your Birth Year'
-        className="p-2 px-2 border border-[#38597c] text-blue-200 rounded mb-4 bg-[#18395f] rounded-lg w-48"
+        className="p-2 px-2 border border-[#38597c] text-blue-50 mb-4 bg-[#18395f] rounded-lg w-68"
       />
-      <button className="bg-[#738aa0] text-red-50 w-48 px-4 py-2 rounded rounded-lg hover:bg-gray-200">
+      <button className="bg-[#738aa0] text-red-50 w-68 px-4 py-2  rounded-lg hover:bg-sky-700">
         Submit
       </button>
     </div>
