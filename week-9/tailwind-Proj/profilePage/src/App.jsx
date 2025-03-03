9a8ca5755c61d7ba46bc0734ec1a6bcde376d6c1
Ashish Kumar <ashishkr45943@gmail.com>
2025-03-03 19:25:24 +0530

    Initali commit

---
+++ week-9/tailwind-Proj/profilePage/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { SideBar } from './Components/SideBar'
+
+function App() {
+  return (
+    <>
+      <div>
+        <SideBar />
+      </div>
+    </>
+  )
+}
+
+export default App;
---
+++ week-9/tailwind-Proj/profilePage/src/Components/SideBar.jsx
@@ -0,0 +1,6 @@
+export function SideBar() {
+	return <div className="flex">
+		<div className="bg-red-300">5ideBar</div>
+		<div className="bg-green-400">content</div>
+	</div>
+}
